28a2d236d7d3b04fe260d890900ded553e855350
bynexse <christoffer.alsbjer@gmail.com>
2026-08-06 19:39:30 +0200

    feat: publish crawler rules for the public Bynex site

---
+++ app/robots.ts
@@ -0,0 +1,32 @@
+import type { MetadataRoute } from "next";
+
+const baseUrl = "https://www.bynex.se";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/account/",
+          "/admin/",
+          "/api/",
+          "/app/",
+          "/ata/",
+          "/auth/",
+          "/avtal/",
+          "/kundportal/",
+          "/login",
+          "/offert/",
+          "/onboarding",
+          "/pilot-login",
+          "/q/",
+          "/signup",
+        ],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
