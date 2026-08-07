dfdc447e730bb669f915dceb2d4e1f903b1983c6
bynexse <christoffer.alsbjer@gmail.com>
2026-08-08 00:16:41 +0200

    feat: surface missing required diary contributions

--- components/modules/time/TimePolicyDiaryPanel.tsx
+++ components/modules/time/TimePolicyDiaryPanel.tsx
@@ -1,3 +1,17 @@
 "use client";
 
-export { default } from "@/components/modules/time/TimePolicyDiaryPanelV2";
+import TimeMissingDiaryPanel from "@/components/modules/time/TimeMissingDiaryPanel";
+import TimePolicyDiaryPanelV2 from "@/components/modules/time/TimePolicyDiaryPanelV2";
+
+export default function TimePolicyDiaryPanel({
+  notify,
+}: {
+  notify: (message: string) => void;
+}) {
+  return (
+    <div className="space-y-5">
+      <TimePolicyDiaryPanelV2 notify={notify} />
+      <TimeMissingDiaryPanel />
+    </div>
+  );
+}
