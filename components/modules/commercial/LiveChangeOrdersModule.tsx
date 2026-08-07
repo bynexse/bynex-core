4fb5d8a992b947b66801607c9e6e719df1f47eed
bynexse <christoffer.alsbjer@gmail.com>
2026-08-07 11:43:16 +0200

    feat: surface ÄTA email recovery in workspace

--- components/modules/commercial/LiveChangeOrdersModule.tsx
+++ components/modules/commercial/LiveChangeOrdersModule.tsx
@@ -1,1 +1,17 @@
-export { default } from "./BynexChangeOrdersWorkspace";
+"use client";
+
+import BynexChangeOrdersWorkspace from "./BynexChangeOrdersWorkspace";
+import ChangeOrderDeliveryRecovery from "./ChangeOrderDeliveryRecovery";
+
+export default function LiveChangeOrdersModule({
+  notify,
+}: {
+  notify: (message: string) => void;
+}) {
+  return (
+    <div className="space-y-5">
+      <ChangeOrderDeliveryRecovery notify={notify} />
+      <BynexChangeOrdersWorkspace notify={notify} />
+    </div>
+  );
+}
